ec3a6f74a04317eb3d4a954b322768a01cc887a9
JeanComeC <185205228+JeanComeC@users.noreply.github.com>
2026-06-11 15:04:04 +0200

    update main.c

--- src/main.c
+++ src/main.c
@@ -3,14 +3,27 @@
 #include "export.h"
 #include <stdio.h>
 
+
 int main(void){
+    //Initialization & Verification
+    if(MAX_POINT<3){
+        perror("Error MAX_POINT isn't valid.\n");
+        exit(1);
+    }
     struct Data_output* tabmain=create_tab_data(MAX_POINT);
 
+    //Calculation
+
+
+
+    //Writting & Export
     if(!writting_json(PATH_EXPORT_JSON,tabmain)){
         perror("Error in writting_json()\n");
         exit(1);
     }
 
+    //Cleaning
     destroy_tab_data(tabmain);
+    printf("Finished ...\n");
     return 0;
 }
