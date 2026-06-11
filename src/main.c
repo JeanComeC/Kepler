fa76c81182626f019b368b1b94d1cd4fbf92b206
JeanComeC <185205228+JeanComeC@users.noreply.github.com>
2026-06-11 12:04:00 +0200

    rename files

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
+#include "physic_calc.h"
 #include "algo.h"
-#include "math.h"
 #include "export.h"
 #include <stdio.h>
 
 int main(void){
     printf("ca compile !\n");
+    double test=12.4e11;
+    printf("test = %.10e\n",test);//10 = nb de chiffres apres la virgule
     return 0;
 }
---
+++ src/physic_calc.c
@@ -0,0 +1,2 @@
+#include "physic_calc.h"
+
