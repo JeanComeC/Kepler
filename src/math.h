dae0dc9be1bb8792326b4ad9948c31d98a155fdc
JeanComeC <185205228+JeanComeC@users.noreply.github.com>
2026-06-11 10:43:15 +0200

    create files

---
+++ src/math.h
@@ -0,0 +1,23 @@
+#ifndef MATH_H
+#define MATH_H
+
+//BIBLIOTHEQUE
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+//MACRO
+
+
+//ENUMERATION
+
+
+//STRUCTURE
+
+
+//PROTOTYPE
+
+
+
+#endif
