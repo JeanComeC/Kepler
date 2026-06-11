28d7b8aaac8702f9dd83ba1c541b59506d671082
JeanComeC <185205228+JeanComeC@users.noreply.github.com>
2026-06-11 22:06:45 +0200

    create functions for calc positions

--- src/physic_calc.h
+++ src/physic_calc.h
@@ -13,15 +13,19 @@
 #define CONSTANTE_GRAVITATION_UNIVERSELLE 6.674184E+11
 #define MASSE_SOLEIL_KG 1.989000E+30
 
-//ENUMERATION
-
+//CONSTANTES GLOBALES
+const struct Coordinates r0 = {.x=0,.y=0,.z=0};
+const struct Coordinates v0 = {.x=0,.y=0,.z=0};
 
 //STRUCTURE
 
 
 //PROTOTYPE
 double calc_norme(struct Coordinates vector);
-struct Coordinates calc_acceleration();
+struct Coordinates calc_acceleration(struct Coordinates vect_r);
+struct Coordinates calc_vitesse(struct Coordinates vect_v, struct Coordinates vect_acc);
+struct Coordinates calc_position(struct Coordinates vect_r, struct Coordinates vect_v);
 
+bool main_calculation(struct Data_output* tabmain);
 
 #endif
