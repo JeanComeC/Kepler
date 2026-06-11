da940593e070447c6cc86606d04f8b3aab079d2b
JeanComeC <185205228+JeanComeC@users.noreply.github.com>
2026-06-11 19:46:54 +0200

    remove rounded

--- src/physic_calc.c
+++ src/physic_calc.c
@@ -1,10 +1,18 @@
 #include "physic_calc.h"
 
-
-
 double calc_norme(struct Coordinates vector){
     double norme=0.0;
     norme=pow(vector.x,2)+pow(vector.y,2)+pow(vector.z,2);
     norme=sqrt(norme);
-    return norme;//l'arrondir ?
+    return norme;
 }
+
+struct Coordinates calc_acceleration(struct Coordinates v_position){
+    struct Coordinates acc;
+    double factor=(CONSTANTE_GRAVITATION_UNIVERSELLE*MASSE_SOLEIL_KG)/pow(calc_norme(v_position),3);
+    acc.x=-1*factor*v_position.x;
+    acc.y=-1*factor*v_position.y;
+    acc.z=-1*factor*v_position.z;
+    return acc;
+}
+
