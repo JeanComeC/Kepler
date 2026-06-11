d359170850c61887ec87bbdabe2a97a5a2b01966
JeanComeC <185205228+JeanComeC@users.noreply.github.com>
2026-06-11 11:37:09 +0200

    create structures + prototype

--- src/algo.h
+++ src/algo.h
@@ -2,22 +2,36 @@
 #define ALGO_H
 
 //BIBLIOTHEQUE
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdint.h>
 
 //MACRO
-
+#define MAX_POINT 8766
 
 //ENUMERATION
 
 
 //STRUCTURE
+struct Coordinates{
+    double x;
+    double y;
+    double z;
+};
 
+struct Data_output{
+    size_t id;
+    double t;
+    struct Coordinates coordinates;
+    double ke;
+    double pe;
+};
 
 //PROTOTYPE
-
+struct Data_output* create_tab_data(size_t size_tab);
+void destroy_tab_data(struct Data_output* tab_data);
 
 
 #endif
