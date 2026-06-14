d03c9a2734b8dba2591a11e8e89751ab4dcede60
JeanComeC <185205228+JeanComeC@users.noreply.github.com>
2026-06-14 15:11:52 +0200

    update main.c

--- src/main.c
+++ src/main.c
@@ -9,11 +9,12 @@
 
 int main(void){
     //Initialization & Verification
-    if(MAX_POINT<3 || MAX_POINT!=(int)((365.25*24.0*3600.0)/H_PAS_TEMPOREL_SECONDE)){
-        perror("Error MAX_POINT isn't valid.\n");
-        exit(1);
-    }
-    //struct Data_output* tabmain=create_tab_data(MAX_POINT);
+    // if(MAX_POINT<3 || MAX_POINT!=(int)((365.25*24.0*3600.0)/H_PAS_TEMPOREL_SECONDE)){
+    //     perror("Error MAX_POINT isn't valid.\n");
+    //     exit(1);
+    // }
+    struct Tab_Earth tab_Earth=create_Tab_Earth();
+    struct Tab_Rocket tab_Rocket=create_Tab_Rocket();
 
     //Calculation
     // if(!main_calculation(tabmain)){
@@ -26,7 +27,8 @@ int main(void){
     
 
     //Cleaning
-    //destroy_tab_data(tabmain);
+    destroy_Tab_Earth(&tab_Earth);
+    destroy_Tab_Rocket(&tab_Rocket);
     printf("Finished ...\n");
     return 0;
 }
