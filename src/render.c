4395245968ea94f996357d1ab5511e4476998099
juleschr <185072902+juleschr@users.noreply.github.com>
2026-06-15 10:42:24 +0200

    add rendet.h render.c : drawStar

--- src/render.c
+++ src/render.c
@@ -1,10 +1,40 @@
 #include "render.h"
+#include <raylib.h>
 
 /*
     === TERRITOIRE DE JULES ===
 */
 
+
+void starInit(struct Stars star[], int n){
+    
+    
+    for(int i = 0; i <= n; i ++){
+        int positionWidth = rand()% WINDOW_WIDTH;
+        int positionHeight = rand()% WINDOW_HEIGHT;
+        star[i].x = positionWidth;
+        star[i].y = positionHeight;
+
+    }
+
+}
+
+void drawSTars(struct Stars star[], int n){
+    for(int i = 0; i < n; i++ ){
+      Drawpixel(star[i].x, star[i].y, (Color){255, 255, 255, 255});
+    }
+}
+
+
 bool main_render(struct Tab_Earth* tab_Earth,struct Tab_Rocket* tab_Rocket){
-    //
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "KEPLER");
+
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground((Color){0, 0, 0, 255});
+        
+    }
+
+
     return true;
 }
--- src/render.h
+++ src/render.h
@@ -5,15 +5,30 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "algo.h"
+#include <raylib.h>
 
 //MACRO
+
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 800
+
+
+//struct 
+
+struct Stars{
+    int x;
+    int y;
+};
+
 
 
 //CONSTANTES GLOBALES
 
 
+
 //PROTOTYPE
 bool main_render(struct Tab_Earth* tab_Earth,struct Tab_Rocket* tab_Rocket);
-
+void starInit(struct Stars star[], int n);
+void drawStars(struct Stars star[], int n);
 
 #endif
