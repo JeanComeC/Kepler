81ff764c9d80442da0bb7480f4b3a813834c972b
juleschr <185072902+juleschr@users.noreply.github.com>
2026-06-15 16:24:58 +0200

    dessin de la terre

--- src/render.c
+++ src/render.c
@@ -34,6 +34,12 @@ void drawStars(struct Coordinates star[], int n){
     }
 }
 
+void drawEarth(struct Tab_Earth* tab, int i, struct Coordinates origin, double scale){
+    double px = origin.x + tab->data[i].position.x * scale;
+    double py = origin.y - tab->data[i].position.y * scale;
+    DrawCircle((int)px, (int)py, 5, BLUE);
+}
+
 
 bool main_render(struct Tab_Earth* tab_Earth,struct Tab_Rocket* tab_Rocket){
 
@@ -43,39 +49,41 @@ bool main_render(struct Tab_Earth* tab_Earth,struct Tab_Rocket* tab_Rocket){
     struct Coordinates origin;
     origin.x = 960;
     origin.y = 540;
-    bool en_saisie = true;
-    struct InputState input = {"", "", 0};
-    struct Coordinates v0 = {0};
-
+    //bool en_saisie = true;
+    // struct InputState input = {"", "", 0};
+    // struct Coordinates v0 = {0};
+    int i = 0;
+    double scale = 500.0 / 1.47e+11;
     while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground((Color){0, 0, 0, 255});
         drawStars(stars, 1080);
         drawSun(origin, 5);
-        if(en_saisie){
-            DrawText("Entrez vx:", 100, 100, 20, WHITE);
-            DrawText(input.vx, 300, 100, 20, GREEN);
+        drawEarth(tab_Earth, i, origin, scale);
+        // if(en_saisie){
+        //     DrawText("Entrez vx:", 100, 100, 20, WHITE);
+        //     DrawText(input.vx, 300, 100, 20, GREEN);
 
-            DrawText("Entrez vy:", 100, 150, 20, WHITE);
-            DrawText(input.vy, 300, 150, 20, GREEN);
+        //     DrawText("Entrez vy:", 100, 150, 20, WHITE);
+        //     DrawText(input.vy, 300, 150, 20, GREEN);
 
-            int touche = GetCharPressed();
-            if(touche >= '0' && touche <= '9'){
-            // ajouter le caractère au champ actif
-            if(input.champ_actif == 0){
-                strncat(input.vx, (char[]){touche, 0}, 1);
-            }else {
-                strncat(input.vy, (char[]){touche, 0}, 1);
-            }
-            }
-            if(IsKeyPressed(KEY_TAB)){ input.champ_actif = 1; }
-            if(IsKeyPressed(KEY_ENTER)){ 
-                v0.x = atof(input.vx); // ici jean come 
-                v0.y = atof(input.vy);
-                en_saisie = false;
-            }
+        //     int touche = GetCharPressed();
+        //     if(touche >= '0' && touche <= '9'){
+        //     // ajouter le caractère au champ actif
+        //     if(input.champ_actif == 0){
+        //         strncat(input.vx, (char[]){touche, 0}, 1);
+        //     }else {
+        //         strncat(input.vy, (char[]){touche, 0}, 1);
+        //     }
+        //     }
+        //     if(IsKeyPressed(KEY_TAB)){ input.champ_actif = 1; }
+        //     if(IsKeyPressed(KEY_ENTER)){ 
+        //         v0.x = atof(input.vx); // ici jean come 
+        //         v0.y = atof(input.vy);
+        //         en_saisie = false;
+        //     }
                 
-        }
+        //}
         EndDrawing();
     
     }
