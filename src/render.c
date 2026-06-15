488983c2146313e6073341d5002f74b8847606c0
juleschr <185072902+juleschr@users.noreply.github.com>
2026-06-15 11:25:43 +0200

    add render.c render.h

--- src/render.c
+++ src/render.c
@@ -1,23 +1,24 @@
 #include "render.h"
+#include "src/algo.h"
 #include <raylib.h>
 
 
 
 
-void starInit(struct Stars star[], int n){
+void starInit(struct Coordinates stars[], int n){
     
     
     for(int i = 0; i <= n; i ++){
         int positionWidth = rand()% WINDOW_WIDTH;
         int positionHeight = rand()% WINDOW_HEIGHT;
-        star[i].x = positionWidth;
-        star[i].y = positionHeight;
+        stars[i].x = positionWidth;
+        stars[i].y = positionHeight;
 
     }
 
 }
 
-void drawStars(struct Stars star[], int n){
+void drawStars(struct Coordinates star[], int n){
     for(int i = 0; i < n; i++ ){
       DrawPixel(star[i].x, star[i].y, (Color){255, 255, 255, 255});
     }
@@ -26,12 +27,12 @@ void drawStars(struct Stars star[], int n){
 
 bool main_render(struct Tab_Earth* tab_Earth,struct Tab_Rocket* tab_Rocket){
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "KEPLER");
-    struct Stars stars[200];
-    starInit(stars, 200);
+    struct Coordinates stars[1080];
+    starInit(stars, 1080);
     while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground((Color){0, 0, 0, 255});
-        drawStars(stars, 200);
+        drawStars(stars, 1080);
 
         EndDrawing();
     }
