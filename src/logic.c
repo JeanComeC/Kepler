f9d5b389b13c7f98b537c16b595d97da4ee19c54
JeanComeC <185205228+JeanComeC@users.noreply.github.com>
2026-06-15 14:48:55 +0200

    update stop_condition()

--- src/logic.c
+++ src/logic.c
@@ -2,6 +2,7 @@
 #include "src/algo.h"
 #include "src/physic_calc.h"
 
+
 struct Coordinates init_random_position(){
     struct Coordinates position_Rocket_output={0};
     position_Rocket_output.x=random_double_for_position();
@@ -10,17 +11,32 @@ struct Coordinates init_random_position(){
     return position_Rocket_output;
 }
 
-bool stop_condition(struct Data_Earth data_Earth, struct Data_Rocket data_Rocket,size_t nb_pas){
+bool stop_condition(enum Code_exit* code_exit, struct Data_Earth data_Earth, struct Data_Rocket data_Rocket,size_t nb_pas){
     //distance fusée-soleil trop petite:
-    if(calc_norme(data_Rocket.position)<DISTANCE_MAX_NUAGE)return true;
+    if(calc_norme(data_Rocket.position)<DISTANCE_MAX_NUAGE){
+        *code_exit=CRASH_SOLAIRE;
+        return true;
+    }
     //distance fusée-soleil trop grande:
-    if(calc_norme(data_Rocket.position)>DISTANCE_MAX_UNIVERS)return true;
+    if(calc_norme(data_Rocket.position)>DISTANCE_MAX_UNIVERS){
+        *code_exit=SORTIE_VIDE_SPATIAL;
+        return true;
+    }
     //distance fusée-terre faible:
     if(calc_norme((struct Coordinates){.x=(data_Rocket.position.x-data_Earth.position.x),.y=(data_Rocket.position.y-data_Earth.position.y),.z=(data_Rocket.position.z-data_Earth.position.z)})<DISTANCE_MAX_NUAGE){
-        if()
+        //si la vitesse est trop grande:
+        if(calc_norme(calc_vitesse_relative(data_Earth.vitesse,data_Rocket.vitesse))>NORME_VITESSE_MAX_IMPACT){
+            *code_exit=CRASH_TERRESTRE;
+        }else{
+            *code_exit=ATTERRISSAGE;
+        }
+        return true;
     }
     //durée trop longue:
-    if(nb_pas==10)return true;
+    if(nb_pas>=NB_PAS_MAX){
+        *code_exit=PANNE_CARBURANT;
+        return true;
+    }
     return false;
 }
 
@@ -30,7 +46,8 @@ bool stop_condition(struct Data_Earth data_Earth, struct Data_Rocket data_Rocket
 bool main_logic(struct Tab_Earth* tab_Earth,struct Tab_Rocket* tab_Rocket){
     struct Data_Earth data_Earth={R0_EARTH,V0_EARTH};
     struct Data_Rocket data_Rocket={/*fonction qui recupere la réponse du joueur*/{0},init_random_position()};
-    while(!stop_condition(data_Earth,data_Rocket,tab_Rocket->size)){
+    enum Code_exit code_exit;
+    while(!stop_condition(&code_exit,data_Earth,data_Rocket,tab_Rocket->size)){
         struct Data_Earth data_Earth_news=calc_Data_Earth(data_Earth);
         struct Data_Rocket data_Rocket_news=calc_Data_Rocket(data_Rocket);
         //Filling
